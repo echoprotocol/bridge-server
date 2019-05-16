ed56d30936bbe5a35e58ba0386979b89d90ecf60
dmitry <d.karpushkin@pixelplex.io>
2019-05-16 18:58:34 +0300

    Init

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import * as config from 'config';
+import * as log4js from 'log4js';
+import { initInitableHelpers, initModule } from './_awilix';
+
+const logger = log4js.getLogger();
+logger.level = config.logger.level || 'info'; // FIXME: what for is config.logger.level?
+
+async function main() {
+	try {
+		await initInitableHelpers();
+		const moduleName = process.env.MODULE;
+		logger.trace(`${moduleName} module initializing`);
+		await initModule(process.env.MODULE);
+	} catch (error) {
+		logger.error(error);
+	}
+}
+
+main();
